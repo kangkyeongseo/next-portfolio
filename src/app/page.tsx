630e2c26df0dbb39d2485f9ed6021151694243ff
kangkyeongseo <kks_big@naver.com>
2023-12-11 16:04:23 +0900

    feat: Wave

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/Header";
+import Wave from "@/components/Wave";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="mt-24">
-        <h1>portfolio</h1>
+      <div className="w-100 h-[100vh] pt-24">
+        <Wave />
       </div>
     </>
   );
