6a82e3e6b525e7da0c9637cdae260772474dbbf4
kangkyeongseo <kks_big@naver.com>
2023-12-14 17:20:30 +0900

    feat: Header 컴포넌트 Section 이동 기능 추가

--- src/components/laytout/Header.tsx
+++ src/components/laytout/Header.tsx
@@ -1,12 +1,42 @@
+"use client";
+
+import { useSectionMove } from "@/hooks/useSectionMove";
+
 export default function Header() {
+  const { handleSectionMove } = useSectionMove();
   return (
-    <header className="w-full fixed top-0 flex justify-between px-24 py-8">
-      <h1 className="text-lg uppercase text-gray-600">KyeongSeo</h1>
+    <header className="z-10 w-full fixed top-0 flex justify-between px-24 py-8 bg-[rgba(255,255,255,0.8)] ">
+      <h1
+        className="cursor-pointer text-lg uppercase text-gray-600 hover:text-gray-900"
+        onClick={() => handleSectionMove("intro")}
+      >
+        KyeongSeo
+      </h1>
       <ul className="flex space-x-8 uppercase text-gray-500">
-        <li>About</li>
-        <li>Skill</li>
-        <li>Project</li>
-        <li>Contact</li>
+        <li
+          className="cursor-pointer hover:text-gray-900"
+          onClick={() => handleSectionMove("about")}
+        >
+          About
+        </li>
+        <li
+          className="cursor-pointer hover:text-gray-900"
+          onClick={() => handleSectionMove("skill")}
+        >
+          Skill
+        </li>
+        <li
+          className="cursor-pointer hover:text-gray-900"
+          onClick={() => handleSectionMove("project")}
+        >
+          Project
+        </li>
+        <li
+          className="cursor-pointer hover:text-gray-900"
+          onClick={() => handleSectionMove("contact")}
+        >
+          Contact
+        </li>
       </ul>
     </header>
   );
