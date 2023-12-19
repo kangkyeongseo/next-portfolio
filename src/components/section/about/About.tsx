961e827d649c3ab0afb58b73637848589b127f97
kangkyeongseo <kks_big@naver.com>
2023-12-19 11:34:34 +0900

    add: Info 컴포넌트 추가

--- src/components/section/about/About.tsx
+++ src/components/section/about/About.tsx
@@ -1,5 +1,15 @@
 import Section from "../Section";
+import Info from "./Info";
 
 export default function About() {
-  return <Section sectionId="about">about</Section>;
+  return (
+    <Section sectionId="about">
+      <div className="w-full h-full flex flex-col justify-center items-center gap-5">
+        <Info type="name" value="강경서" />
+        <Info type="birth" value="1994.07.18" />
+        <Info type="phone" value="010-6415-5062" />
+        <Info type="mail" value="kks_big@naver.com" />
+      </div>
+    </Section>
+  );
 }
